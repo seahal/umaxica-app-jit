cbac1e41b50053d53ea7c4ddad4eaa347abccca9
seahal <seahal.com@gmail.com>
2025-03-13 18:52:43 +0900

    [merge] merging...

--- test/mailers/net/user_registration_mailer_test.rb
+++ test/mailers/net/user_registration_mailer_test.rb
@@ -4,8 +4,8 @@ class Net::UserRegistrationMailerTest < ActionMailer::TestCase
   test "welcome" do
     mail = Net::UserRegistrationMailer.welcome
     assert_equal "Welcome", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal [ "to@example.org" ], mail.to
+    assert_equal [ "from@example.com" ], mail.from
     assert_match "Hi", mail.body.encoded
   end
 end
