33ee37fd5b0ddd08aabcb5a76ee18a0743091391
seahal <seahal.com@gmail.com>
2025-03-21 12:21:36 +0900

    [REFACTORING] ...

--- app/controllers/www/app/registration/emails_controller.rb
+++ app/controllers/www/app/registration/emails_controller.rb
@@ -13,8 +13,8 @@ module Www
           res = cloudflare_turnstile_validation
 
           respond_to do |format|
-            if res['success'] == true && @user_email.save
-               format.html { redirect_to www_app_registration_email_path(Base64::urlsafe_encode64(@user_email.address)), notice: "Sample was successfully created." }
+            if res["success"] == true && @user_email.save
+               format.html { redirect_to www_app_registration_email_path(Base64.urlsafe_encode64(@user_email.address)), notice: "Sample was successfully created." }
             else
               format.html { render :new, status: :unprocessable_entity }
             end
@@ -36,7 +36,7 @@ module Www
 
         # Only allow a list of trusted parameters through.
         def sample_params
-          params.expect(user_email: [:address, :confirm_policy])
+          params.expect(user_email: [ :address, :confirm_policy ])
         end
 
         def cloudflare_turnstile_validation
@@ -45,7 +45,7 @@ module Www
                                 "response" => params["cf-turnstile-response"],
                                 "remoteip" => request.remote_ip })
 
-          JSON::parse(res.body)
+          JSON.parse(res.body)
         end
       end
     end
