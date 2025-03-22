58569ab3aebbbd8884f56d29b8e4ea639a8cacd4
seahal <seahal.com@gmail.com>
2025-03-22 11:08:55 +0900

    Revert "I'm working on implementing a function for email delivery with Amazon SES."
    This reverts commit 53cc0e9be1f073f49d9e562efbeb6d4e0c939ab9.

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: "sample@email.umaxica.app"
-
-  def welcome_email
-    @user = params[:user]
-    @url  = "http://example.com/login"
-    mail(to: 'm.shiihara@email.umaxica.app', subject: "私の素敵なサイトへようこそ")
-  end
-end
