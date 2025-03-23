3735a973df21ee5f7017985ebede2ea53218a6cf
seahal <seahal.com@gmail.com>
2025-03-23 19:22:53 +0900

    [update] misc(10)

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -2,7 +2,7 @@ require "opentelemetry/sdk"
 require "opentelemetry/exporter/otlp"
 require "opentelemetry/instrumentation/all"
 
-unless Rails.env.test?
+if !Rails.env.test? && !ENV["CI"]
   OpenTelemetry::SDK.configure do |c|
     c.service_name = "umaxica-app-jit"
     c.use_all # enables all instrumentation!
