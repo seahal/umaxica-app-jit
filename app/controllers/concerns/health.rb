8b981a8f57e1a2535c03bdef6706b7ea4b00a413
seahal <seahal.com@gmail.com>
2025-03-30 22:50:09 +0900

    [system] test

--- app/controllers/concerns/health.rb
+++ app/controllers/concerns/health.rb
@@ -7,16 +7,16 @@ module Health
     expires_in 1.second, public: true # this page wouldn't include private data
 
     # FIXME: much more validations requires
-    @status, @body =  if !!User.connection.execute("SELECT 1 FROM users LIMIT 1")
-                        [ 200, "OK" ]
-    else
-                        [ 500, "NG" ]
-    end
+    @status, @body = if !!User.connection.execute("SELECT 1 FROM users LIMIT 1")
+                       [200, "OK"]
+                     else
+                       [500, "NG"]
+                     end
 
     case request.path
     when "/health"
       render html: @body, status: @status
-    when  "/v1/health"
+    when "/v1/health"
       render json: { status: @body }, status: @status
     else
       raise
