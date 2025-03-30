1f891f4e82a5d055e9c5c1c5a6f33cb393b9f9b8
seahal <seahal.com@gmail.com>
2025-03-31 07:00:07 +0900

    [SYSTEM] introduced React

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "./components/sample"
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './components/App'
+
+const dom = document.getElementById('root')
+
+ReactDOM.createRoot(dom).render(
+    <React.StrictMode>
+        <App name='Bun' />
+    </React.StrictMode>
+)
