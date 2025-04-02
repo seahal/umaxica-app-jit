d09e04fcf65db26f8b8e08952daeaab96341a15b
seahal <seahal.com@gmail.com>
2025-04-02 20:49:39 +0900

    [update] fixed broken tests.

--- test/controllers/www/app/preferences_controller_test.rb
+++ test/controllers/www/app/preferences_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Www::App::PreferencesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get new_www_app_preferences_url
+    get www_app_preference_url
     assert_response :success
   end
 end
