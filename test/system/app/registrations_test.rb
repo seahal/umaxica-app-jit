37729cbc6e424e54a5ff88a228c78502a2acb604
seahal <seahal@gmail.com>
2025-04-19 12:23:24 +0900

    refactor: update module names from Net to App in registration tests and views

--- test/system/app/registrations_test.rb
+++ test/system/app/registrations_test.rb
@@ -1,12 +1,12 @@
-# # frozen_string_literal: true
-#
-# require "application_system_test_case"
-#
-# module Net
-#   class HealthsTest < ApplicationSystemTestCase
-#     test "visiting the root of registration index" do
-#       visit new_www_app_registration_url
-#       assert_selector "h1", text: "Net::Registrations"
-#     end
-#   end
-# end
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+module App
+  class HealthsTest < ApplicationSystemTestCase
+    test "visiting the root of registration index" do
+      visit www_app_root_url
+      assert_selector "h1", text: "Www::App::Roots#index"
+    end
+  end
+end
