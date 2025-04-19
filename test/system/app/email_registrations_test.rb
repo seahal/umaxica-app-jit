b42d4066e5e04e3ddf57e50d1f81a3587828b79d
seahal <seahal@gmail.com>
2025-04-19 12:41:31 +0900

    [UPDATE] fixed some tests.

--- test/system/app/email_registrations_test.rb
+++ test/system/app/email_registrations_test.rb
@@ -4,13 +4,12 @@ require "application_system_test_case"
 
 module App
   class EmailRegistrationTest < ApplicationSystemTestCase
-    # test "visiting the root of email registration index" do
-    #   visit new_app_registration_email_url
-    #   assert_selector "h1", text: "Net::RegistrationEmails#new"
-    #   assert_selector "label", text: "Address"
-    #   assert_selector "label", text: "利用規約に同意する"
-    # end
-    #
+    test "visiting the root of email registration index" do
+      visit new_www_app_registration_email_url
+      assert_selector "label", text: "Address"
+      assert_selector "label", text: "利用規約に同意する"
+    end
+
     # test "fill and actions" do
     #   visit new_net_registration_email_url
     #   assert_selector "h1", text: "Net::RegistrationEmails#new"
