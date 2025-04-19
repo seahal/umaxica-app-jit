72f8c49753c4d9cb883be31d7da385a58c833379
seahal <seahal@gmail.com>
2025-04-19 23:05:42 +0900

    [UPDATE] misc.

--- test/controllers/www/app/session/passkeys_controller_test.rb
+++ test/controllers/www/app/session/passkeys_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class Www::App::Session::PasskeysControllerTest < ActionDispatch::IntegrationTest
-  # test "should get new" do
-  #   get new_net_session_passkey_url
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get new_www_app_session_passkey_url
+    assert_response :success
+  end
 end
