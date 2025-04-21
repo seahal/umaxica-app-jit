d5978c07fce5bff5721d42b11c7bc715595c18d7
seahal <seahal@gmail.com>
2025-04-22 00:40:18 +0900

    refactor: adjust comments for time_based_one_time_password_id in migration files

--- db/tokens_migrate/20240830163800_create_staff_tokens.rb
+++ db/tokens_migrate/20240830163800_create_staff_tokens.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class CreateStaffTokens < ActiveRecord::Migration[7.2]
-    # def change
-    #   create_table :staff_tokens, id: :uuid do |t|
-    #     t.references :staff, null: false, foreign_key: true, type: :uuid
-    #
-    #     t.timestamps
-    #   end
-    # end
+  # def change
+  #   create_table :staff_tokens, id: :uuid do |t|
+  #     t.references :staff, null: false, foreign_key: true, type: :uuid
+  #
+  #     t.timestamps
+  #   end
+  # end
 end
