d78f6735efd35987b70bf33daa645735e36740cd
seahal <seahal@gmail.com>
2025-04-21 20:25:56 +0900

    [UPDATE] enhance TimeBasedOneTimePassword model with validations and update test cases; configure Active Record encryption in test environment

--- app/models/time_based_one_time_password.rb
+++ app/models/time_based_one_time_password.rb
@@ -1,3 +1,9 @@
 class TimeBasedOneTimePassword < AccountsRecord
+  attr_accessor :first_token, :second_token
   encrypts :private_key
+
+  #
+  validates :private_key, presence: true
+  validates :first_token, presence: true, length: { is: 6 }, numericality: { only_integer: true }
+  validates :second_token, presence: true, length: { is: 6 }, numericality: { only_integer: true }
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,5 +61,8 @@ Rails.application.configure do
     config.assets.gzip = false
   end
 
-  # ???
+  ## Active Record Encryption
+  config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+  config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+  config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
 end
