5cc388289347db6ea43a16b98d733cc2e4537804
seahal <seahal@gmail.com>
2025-04-23 04:34:51 +0900

    [UPDATE] renaming...

--- app/services/identifier_service.rb
+++
@@ -1,4 +0,0 @@
-# INFO: This service is powered by a single, globally unique database.
-
-class IdentifierService
-end
---
+++ app/services/universal_service.rb
@@ -0,0 +1,4 @@
+# INFO: This service is powered by a universal, globally unique database.
+
+class UniversalService
+end
