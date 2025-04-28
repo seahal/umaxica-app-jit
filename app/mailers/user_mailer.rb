a9bb96addf713d77a4ab590450db0df7e4c1ae02
seahal <seahal@gmail.com>
2025-04-29 08:40:56 +0900

    [SYSTEM] migrated smtp server from aws to resend.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome_email.subject
+  def welcome_email
+    @greeting = "Hi"
+
+    mail to: ""
+  end
+end
