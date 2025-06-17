ecebf1a7a3b32c792c09bd0b85a6fccdcb80d08a
seahal <seahal.com@gmail.com>
2025-06-18 03:36:13 +0900

    [update] refactor inquiries views and controller, removing unused files and adding new inquiry forms

--- app/controllers/www/app/inquiries_controller.rb
+++ app/controllers/www/app/inquiries_controller.rb
@@ -2,10 +2,11 @@ module Www::App
   class InquiriesController < ApplicationController
     include ::Cloudflare
     include ::Rotp
-    include ::Common
-    include ::Memorize
+    # include ::Common
+    # include ::Memorize
 
     def new
+      @service_site_contact = ServiceSiteContact.new
     end
 
     def create
