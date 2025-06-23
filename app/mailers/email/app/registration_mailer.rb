ace4a5e9e7cddbe06fa33b002a20ffb4989d18e7
seahal <seahal.com@gmail.com>
2025-06-23 13:48:57 +0900

    add registration mailer and associated views for email notifications

--- app/mailers/email/app/registration_mailer.rb
+++ app/mailers/email/app/registration_mailer.rb
@@ -1,5 +1,5 @@
 module Email::App
-  class EmailRegistrationMailer < ApplicationMailer
+  class RegistrationMailer < ApplicationMailer
     # Subject can be set in your I18n file at config/locales/en.yml
     # with the following lookup:
     #
