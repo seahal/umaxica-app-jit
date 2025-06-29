b743481345e4302f218742c26c9f41711a8b8c2c
seahal <seahal.com@gmail.com>
2025-06-29 21:00:44 +0900

    [...]

--- src/application.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import HelloWorld from './HelloWorld';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const root = ReactDOM.createRoot(document.getElementById('react-root'));
-  root.render(<HelloWorld />);
-});
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,19 @@
 import { defineConfig } from 'vite'
 
 
-export default defineConfig
-({
-  server
-: {
-    cors
-: {
-    origin
-    : 'http://dist.net.localhost:3000/',
+export default defineConfig({
+  server: {
+    cors: {
+      // ブラウザ経由でアクセスするオリジン
+      origin: 'http://my-backend.example.com',
     },
   },
-  build
-: {
+  build: {
     // outDir に .vite/manifest.json を出力
-    manifest
-: true,
-    rollupOptions
-: {
+    manifest: true,
+    rollupOptions: {
       // デフォルトの .html エントリーを上書き
-      input
-: '/dist/main.js',
+      input: './src/application.tsx',
     },
   },
 })
