2eae376d787e9cc2c796eaba5f9143363b992eac
seahal <seahal.com@gmail.com>
2025-06-30 19:56:36 +0900

    [misc] ........

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -3,9 +3,9 @@ import HelloWorld from "./components/HelloWorld";
 import EmailAddress from "./components/www/app/email_address";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const email_address = document.getElementById("email_address");
-    if (email_address) {
-        const emailRoot = createRoot(email_address);
-        emailRoot.render(<EmailAddress />);
-    }
+	const email_address = document.getElementById("email_address");
+	if (email_address) {
+		const emailRoot = createRoot(email_address);
+		emailRoot.render(<EmailAddress />);
+	}
 });
