146f6d071092f95efabafba1c72a1c41b368c95d
seahal <seahal.com@gmail.com>
2025-07-08 03:45:24 +0900

    [misc] ......

--- app/javascript/controllers/www/app/inquiry/before_submit.js
+++ app/javascript/controllers/www/app/inquiry/before_submit.js
@@ -17,12 +17,31 @@ const service_site_contact_telephone_number = document.querySelector(
 );
 
 submitButton.addEventListener("click", (e) => {
+	validness.service_site_contact_confirm_policy = isCheckedConfirmPolicy(
+		service_site_contact_confirm_policy,
+	);
+	validness.service_site_contact_email_address = isValidEmailAddress(
+		service_site_contact_email_address,
+	);
+	validness.service_site_contact_telephone_number = isValidTelephoneNumber(
+		service_site_contact_telephone_number,
+	);
+
 	if (Object.values(validness).every((value) => value === true)) {
-		alert("Hello");
-		// 条件が満たされたらフォームを送信したい場合、e.preventDefault() をここに置かない
-		// または、特定の条件でのみ e.preventDefault() を実行する
-	} else {
 		e.preventDefault();
-		alert("Please check the form");
+	} else {
+		alert("invalid values were inputted!");
 	}
 });
+
+function isCheckedConfirmPolicy(element) {
+	return !!element.checked;
+}
+
+function isValidEmailAddress(_element) {
+	return true;
+}
+
+function isValidTelephoneNumber(_element) {
+	return true;
+}
