3473e046c02845a061ca93269c2694c66075ac11
seahal <seahal.com@gmail.com>
2025-07-09 09:01:23 +0900

    [FEATURE] Add docs and news endpoints.

---
+++ app/models/user_webauthn_credential.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class UserWebauthnCredential < IdentifiersRecord
+  belongs_to :user
+
+  validates :external_id, presence: true, uniqueness: true
+  validates :public_key, presence: true
+  validates :nickname, presence: true
+  validates :sign_count, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  # WebAuthn認証器のタイプ
+  enum authenticator_type: {
+    platform: 0,      # Touch ID, Face ID, Windows Hello等
+    roaming: 1        # YubiKey, セキュリティキー等
+  }
+
+  scope :active, -> { where(active: true) }
+  
+  def increment_sign_count!
+    increment!(:sign_count)
+  end
+
+  def deactivate!
+    update!(active: false)
+  end
+end
