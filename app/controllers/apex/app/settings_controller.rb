964941296735c92deed3745c43e212d7475fc771
seahal <seahal.com@gmail.com>
2025-08-10 12:24:19 +0900

    [info] remove unused controllers and their corresponding tests

--- app/controllers/apex/app/settings_controller.rb
+++
@@ -1,7 +0,0 @@
-module Apex
-  module App
-    class SettingsController < ApplicationController
-      def show; end
-    end
-  end
-end
--- app/controllers/apex/app/stagings_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Apex
-  module App
-    class StagingsController < ApplicationController
-      include ::Staging
-    end
-  end
-end
