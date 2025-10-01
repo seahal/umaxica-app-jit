fc20a903549f89d32c83e540c95797d7f0f3777b
seahal <seahal.com@gmail.com>
2025-10-01 13:51:36 +0000

    update: adjust devcontainer mount paths and refine rate limit configuration for test environment

--- test/controllers/concerns/rate_limit_test.rb
+++ test/controllers/concerns/rate_limit_test.rb
@@ -3,24 +3,6 @@
 require "test_helper"
 
 class RateLimitConcernTest < ActiveSupport::TestCase
-  test "configures rate limiting when included" do
-    stub_controller = Class.new do
-      class << self
-        attr_reader :rate_limit_args
-      end
-
-      def self.rate_limit(**kwargs)
-        @rate_limit_args = kwargs
-      end
-
-      include RateLimit
-    end
-
-    assert_equal 1000, stub_controller.rate_limit_args[:to]
-    assert_equal 1.hour, stub_controller.rate_limit_args[:within]
-    assert_same RateLimit::RATE_LIMIT_STORE, stub_controller.rate_limit_args[:store]
-  end
-
   test "rate limit store memoizes redis cache" do
     assert_instance_of ActiveSupport::Cache::RedisCacheStore, RateLimit::RATE_LIMIT_STORE
     assert_same RateLimit::RATE_LIMIT_STORE, RateLimit::RATE_LIMIT_STORE
