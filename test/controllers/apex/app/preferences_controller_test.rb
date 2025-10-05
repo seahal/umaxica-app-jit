8a0f94bc2723ee8d9d61c3be035cf13152a110a2
seahal <seahal.com@gmail.com>
2025-10-05 15:20:07 +0900

    Remove unused preference forms, controllers, and tests for email, theme, language, and timezone settings

--- test/controllers/apex/app/preferences_controller_test.rb
+++ test/controllers/apex/app/preferences_controller_test.rb
@@ -12,7 +12,5 @@ class Apex::App::PreferencesControllerTest < ActionDispatch::IntegrationTest
     assert_select "h1", text: I18n.t("apex.app.preferences.title")
     assert_select "a", text: I18n.t("apex.app.preferences.cookie_settings")
     assert_select "a", text: I18n.t("apex.app.preferences.region_settings")
-    assert_select "a", text: I18n.t("apex.app.preferences.email_preferences")
-    assert_select "a", text: I18n.t("apex.app.preferences.theme_settings")
   end
 end
--- test/controllers/apex/com/preferences_controller_test.rb
+++ test/controllers/apex/com/preferences_controller_test.rb
@@ -12,7 +12,5 @@ class Apex::Com::PreferencesControllerTest < ActionDispatch::IntegrationTest
     assert_select "h1", text: I18n.t("apex.com.preferences.title")
     assert_select "a", text: I18n.t("apex.com.preferences.cookie_settings")
     assert_select "a", text: I18n.t("apex.com.preferences.region_settings")
-    assert_select "a", text: I18n.t("apex.com.preferences.email_preferences")
-    assert_select "a", text: I18n.t("apex.com.preferences.theme_settings")
   end
 end
