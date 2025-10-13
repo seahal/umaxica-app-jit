48dbe15c4cf0e0971a3aedb2c229cc00479250e2
seahal <seahal.com@gmail.com>
2025-10-14 01:36:59 +0900

    [Update] Add "necessary cookies" preference, update views, controllers, tests, and localization files. Upgrade dependencies.

--- test/controllers/apex/org/preference/cookies_controller_test.rb
+++ test/controllers/apex/org/preference/cookies_controller_test.rb
@@ -26,6 +26,7 @@ class Apex::Org::Preference::CookiesControllerTest < ActionDispatch::Integration
     assert_select "div#hello-world-component", count: 0
     assert_select "form[action=?]", apex_org_preference_cookie_url do
       assert_select "input[type='hidden'][name='authenticity_token']", count: 1
+      assert_select "input[type='checkbox'][name='accept_necessary_cookies']", count: 1
       assert_select "input[type='checkbox'][name='accept_functional_cookies']", count: 1
       assert_select "input[type='checkbox'][name='accept_performance_cookies']", count: 1
       assert_select "input[type='checkbox'][name='accept_targeting_cookies']", count: 1
@@ -34,6 +35,7 @@ class Apex::Org::Preference::CookiesControllerTest < ActionDispatch::Integration
       assert_select "label", I18n.t("apex.org.preference.cookie.edit.accept_functional_cookies")
       assert_select "label", I18n.t("apex.org.preference.cookie.edit.accept_performance_cookies")
       assert_select "label", I18n.t("apex.org.preference.cookie.edit.accept_targeting_cookies")
+      assert_select "label", I18n.t("apex.org.preference.cookie.edit.accept_necessary_cookies")
       assert_select "label", I18n.t("apex.org.preference.cookie.edit.accept_functional_cookies")
       assert_select "label", I18n.t("apex.org.preference.cookie.edit.accept_performance_cookies")
       assert_select "label", I18n.t("apex.org.preference.cookie.edit.accept_targeting_cookies")
