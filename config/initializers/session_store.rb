1c8651d3750e201b0ddfe875cea2c8ebad9a0fe9
seahal <m.shiihara@gmail.com>
2025-10-27 13:06:43 +0900

    [checkpoint] misc.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store,
-                                       expire_after: 12.hours,
+                                       expire_after: 14.days,
                                        key: Rails.env.production? ? "__Secure-jit_session" : "jit_session",
                                        secure: Rails.env.production?,
                                        httponly: true,
