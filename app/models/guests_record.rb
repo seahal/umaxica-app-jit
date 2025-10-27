cd8aaf4a8b73a27f9b3fd570adb63557f46d3082
seahal <m.shiihara@gmail.com>
2025-10-27 14:20:10 +0900

    [refactor] renamed the database table from customer to guest. (re2

---
+++ app/models/guests_record.rb
@@ -0,0 +1,5 @@
+class GuestsRecord < ApplicationRecord
+  self.abstract_class = true
+
+  connects_to database: { writing: :guest, reading: :guest_replica }
+end
---
+++ test/models/guests_record_test.rb
@@ -0,0 +1,45 @@
+require "test_helper"
+
+class GuestsRecordTest < ActiveSupport::TestCase
+  test "should be abstract class" do
+    assert GuestsRecord.abstract_class?
+  end
+
+  test "should inherit from ApplicationRecord" do
+    assert GuestsRecord < ApplicationRecord
+  end
+
+  test "should connect to guest database" do
+    # Test that the model is configured to use the guest database
+    # Note: This is a basic structural test
+    assert_respond_to GuestsRecord, :connection_db_config
+  end
+
+  test "should have correct database configuration" do
+    config = GuestsRecord.connection_db_config
+    assert_not_nil config
+  end
+
+  test "should connect to correct writing database" do
+    # Verify the writing database configuration
+    writing_config = GuestsRecord.connection_specification_name
+    assert_not_nil writing_config
+  end
+
+  test "should not be instantiable as abstract class" do
+    assert_raises(NotImplementedError) do
+      GuestsRecord.new
+    end
+  end
+
+  test "should have database connection methods" do
+    assert_respond_to GuestsRecord, :connection
+    assert_respond_to GuestsRecord, :connected?
+  end
+
+  test "should inherit ActiveRecord methods" do
+    assert_respond_to GuestsRecord, :find_by_sql
+    assert_respond_to GuestsRecord, :table_name
+    assert_respond_to GuestsRecord, :primary_key
+  end
+end
