49fc5af1cc477421e31ee646215b81571a6977e2
seahal <m.shiihara@gmail.com>
2025-11-07 04:55:44 +0000

    [update] Refactor environment variable names in integration.yml and remove unused dependencies from package.json and bun.lock

--- app/controllers/help/com/contacts_controller.rb
+++ app/controllers/help/com/contacts_controller.rb
@@ -1,15 +1,16 @@
 module Help
   module Com
     class ContactsController < ApplicationController
+      def show
+      end
+
       def new
         @service_site_contact = ServiceSiteContact.new
         @contact_categories = ContactCategory.order(:title)
       end
 
-      def create
-      end
 
-      def show
+      def create
       end
     end
   end
