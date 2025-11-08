60cc84664d2c13afe29c18191a55ba4b5c4393bd
seahal <m.shiihara@gmail.com>
2025-11-08 10:37:17 +0900

    [update] renamed! (re

---
+++ test/controllers/sign/org/registration/telephones_controller_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class Sign::Org::Registration::TelephonesControllerTest < ActionDispatch::IntegrationTest
+  # test "should get new" do
+  #   get new_sign_org_registration_telephone_url, headers: { "Host" => ENV["SIGN_STAFF_URL"] }
+  #   assert_response :success
+  #   assert_select "form[action=?][method=?]", expected_action, "post" do
+  #     # Check existence and attributes of telephone input field
+  #     assert_select "label[for=?]", "user_telephone_number"
+  #     assert_select "input[type=?][name=?]", "text", "user_telephone[number]"
+  #     # Check existence and attributes of checkbox
+  #     assert_select "label[for=?]", "user_telephone_confirm_policy"
+  #     assert_select "input[type=?][name=?]", "checkbox", "user_telephone[confirm_policy]"
+  #     assert_select "input[type=?][name=?]", "hidden", "user_telephone[confirm_policy]"
+  #     # Check existence and attributes of checkbox
+  #     assert_select "label[for=?]", "user_telephone_confirm_using_mfa"
+  #     assert_select "input[type=?][name=?]", "hidden", "user_telephone[confirm_using_mfa]"
+  #     assert_select "input[type=?][name=?]", "checkbox", "user_telephone[confirm_using_mfa]"
+  #     # cloudflare tunstile
+  #     assert_select "div.cf-turnstile", 1..1
+  #     # Check existence of submit button
+  #     assert_select "input[type=?]", "submit"
+  #   end
+  #   assert_select "a[href=?]", new_sign_org_registration_path
+  #   assert_select "a[href=?]", new_sign_org_authentication_telephone_path
+  # end
+end
