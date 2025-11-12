f6e63c0b6916b0e3c060a6d10649420aa3490ceb
seahal <m.shiihara@gmail.com>
2025-11-12 15:31:31 +0900

    [fix] misc ..

--- app/models/staff_email_staff.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class StaffEmailStaff < TokensRecord
-  belongs_to :email, foreign_key: true, inverse_of: :staff_email_staffs
-  belongs_to :staff, foreign_key: true, inverse_of: :staff_email_staffs
-end
