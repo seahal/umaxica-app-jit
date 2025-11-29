d0dabeeb4c7470c7a078bec0663e7c1096c32605
seahal <m.shiihara@gmail.com>
2025-11-29 16:56:30 +0000

    [update] added tests.

--- test/models/staff_hmac_based_one_time_password_test.rb
+++ test/models/staff_hmac_based_one_time_password_test.rb
@@ -10,7 +10,38 @@
 require "test_helper"
 
 class StaffHmacBasedOneTimePasswordTest < ActiveSupport::TestCase
-  test "the truth" do
-    skip "TODO: implement staff HOTP model test"
+  test "inherits from IdentitiesRecord" do
+    assert_operator StaffHmacBasedOneTimePassword, :<, IdentitiesRecord
+  end
+
+  test "belongs to staff" do
+    association = StaffHmacBasedOneTimePassword.reflect_on_association(:staff)
+
+    assert_not_nil association
+    assert_equal :belongs_to, association.macro
+  end
+
+  test "belongs to hmac_based_one_time_password" do
+    association = StaffHmacBasedOneTimePassword.reflect_on_association(:hmac_based_one_time_password)
+
+    assert_not_nil association
+    assert_equal :belongs_to, association.macro
+  end
+
+  # test "loads staff and hmac associations from fixtures" do
+  #   record = staff_hmac_based_one_time_passwords(:one)
+
+  #   assert_equal staffs(:one), record.staff
+  #   assert_equal hmac_based_one_time_passwords(:one), record.hmac_based_one_time_password
+  # end
+
+  test "allows assignment of associations before persistence" do
+    staff = staffs(:one)
+    hmac = hmac_based_one_time_passwords(:one)
+
+    record = StaffHmacBasedOneTimePassword.new(staff:, hmac_based_one_time_password: hmac)
+
+    assert_same staff, record.staff
+    assert_same hmac, record.hmac_based_one_time_password
   end
 end
