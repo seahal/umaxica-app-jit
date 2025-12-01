f0d218e750817b06d0ec039ec65c13b2c2d10931
seahal <m.shiihara@gmail.com>
2025-12-01 03:13:37 +0000

    [misc] .

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-console.log("application.js loaded!")
 import "@hotwired/turbo-rails"
 import "controllers"
-console.log("All imports completed!")
