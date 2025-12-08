45254a6bd3ebad08aad8a99632e9457464b74e95
seahal <m.shiihara@gmail.com>
2025-12-08 10:23:04 +0900

    [check point] .

--- app/controllers/help/com/application_controller.rb
+++ app/controllers/help/com/application_controller.rb
@@ -5,8 +5,20 @@ module Help
     class ApplicationController < ActionController::Base
       include ::RateLimit
       include ::DefaultUrlOptions
+      include Pundit::Authorization
 
       allow_browser versions: :modern
+
+      rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+      private
+
+      def user_not_authorized
+        respond_to do |format|
+          format.json { render json: { error: I18n.t("errors.forbidden") }, status: :forbidden }
+          format.any { head :forbidden }
+        end
+      end
     end
   end
 end
