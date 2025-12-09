573fc038684964e978397f3779f0ef9395a76d16
seahal <m.shiihara@gmail.com>
2025-12-09 02:58:44 +0000

    [feat] Enhance withdrawal functionality by adding show and destroy actions for users and staff, and update routes for full resource access

--- app/controllers/sign/app/withdrawals_controller.rb
+++ app/controllers/sign/app/withdrawals_controller.rb
@@ -1,12 +1,17 @@
 module Sign
   module App
     class WithdrawalsController < ApplicationController
-      before_action :authenticate_user!, only: [ :create, :update ]
+      before_action :authenticate_user!, only: [ :show, :create, :update, :destroy ]
 
+      def show
+        # Show withdrawal status page for the current user
+        @withdrawn_at = current_user.withdrawn_at
+      end
       # note: you would surprise to think that new is not good for this method,
       #       but you should think that create delete flag to this
       def new
       end
+
 
       def create
         # Check if user is already withdrawn
@@ -29,6 +34,17 @@ module Sign
           redirect_to sign_app_root_path, notice: t("sign.app.withdrawal.update.recovered")
         else
           redirect_to sign_app_root_path, alert: t("sign.app.withdrawal.update.cannot_recover")
+        end
+      end
+
+      def destroy
+        # Only allow permanent removal if account was previously withdrawn
+        if current_user.withdrawn_at.present?
+          current_user.destroy
+          reset_session
+          redirect_to sign_app_root_path, notice: t("sign.app.withdrawal.destroy.deleted")
+        else
+          redirect_to sign_app_root_path, alert: t("sign.app.withdrawal.destroy.not_withdrawn")
         end
       end
     end
--- app/controllers/sign/org/withdrawals_controller.rb
+++ app/controllers/sign/org/withdrawals_controller.rb
@@ -1,10 +1,15 @@
 module Sign
   module Org
     class WithdrawalsController < ApplicationController
-      before_action :authenticate_staff!, only: :create
+      before_action :authenticate_staff!, only: [ :show, :create, :update, :destroy ]
 
+      def show
+        # Show withdrawal status for the current staff
+        @withdrawn_at = current_staff.withdrawn_at
+      end
       def new
       end
+
 
       def create
         # Check if staff is already withdrawn
@@ -18,6 +23,27 @@ module Sign
         current_staff.update(withdrawn_at: Time.current)
         reset_session
         redirect_to sign_org_root_path, notice: t("sign.org.withdrawal.create.success")
+      end
+
+      def update
+        # Recovery: clear withdrawn_at if within 1 month
+        if current_staff.withdrawn_at.present? && current_staff.withdrawn_at > 1.month.ago
+          current_staff.update(withdrawn_at: nil)
+          redirect_to sign_org_root_path, notice: t("sign.org.withdrawal.update.recovered")
+        else
+          redirect_to sign_org_root_path, alert: t("sign.org.withdrawal.update.cannot_recover")
+        end
+      end
+
+      def destroy
+        # Only allow permanent removal if staff was previously withdrawn
+        if current_staff.withdrawn_at.present?
+          current_staff.destroy
+          reset_session
+          redirect_to sign_org_root_path, notice: t("sign.org.withdrawal.destroy.deleted")
+        else
+          redirect_to sign_org_root_path, alert: t("sign.org.withdrawal.destroy.not_withdrawn")
+        end
       end
     end
   end
