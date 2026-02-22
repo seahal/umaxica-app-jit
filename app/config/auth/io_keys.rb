0e8c44d3b28a3ec12014d7e3cc45d83538ec3f27
seahal <m.shiihara@gmail.com>
2026-02-23 05:48:37 +0900

    [CheckPoint] take a rest.....

--- app/config/auth/io_keys.rb
+++ app/config/auth/io_keys.rb
@@ -38,32 +38,3 @@ module Auth
     end
   end
 end
-
-module Preference
-  module IoKeys
-    SECURE_COOKIE_PREFIX = "__Secure-"
-
-    module Cookies
-      THEME = "jit_ct"
-      LEGACY_THEME = "ct"
-      LANGUAGE = "jit_lx"
-      TIMEZONE = "jit_tz"
-      ACCESS_BASENAME = "jit_preference_access"
-      REFRESH_BASENAME = "jit_preference_refresh"
-      DEVICE_BASENAME = "jit_preference_device_id"
-    end
-
-    module Headers
-      DEVICE_ID = "X-Device-Id"
-    end
-
-    module Params
-      CT = :ct
-      LX = :lx
-      RI = :ri
-      TZ = :tz
-      REFRESH_TOKEN = :refresh_token
-      OPTION_ID = :option_id
-    end
-  end
-end
